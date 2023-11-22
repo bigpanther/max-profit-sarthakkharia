6bb266af44450acd8c64c806a8ca05f6efd8ed5f
sarthakkharia <sarthakkharia@gmail.com>
2023-11-21 18:49:26 -0800

    assignment completed

--- main.go
+++ main.go
@@ -2,5 +2,29 @@ package main
 
 // maxProfit returns the maximum profit possible for the stock
 func maxProfit(stockPricesByDay []int) int {
-	return 0
+	var profit int
+	var grandprofit int
+	var key int
+	grandprofit = 0
+
+	for outerloop := 0; outerloop < len(stockPricesByDay); outerloop++ {
+		if outerloop != key {
+			continue
+		}
+		for innerloop := outerloop + 1; innerloop < len(stockPricesByDay); innerloop++ {
+
+			if stockPricesByDay[innerloop] < stockPricesByDay[outerloop] {
+				key = innerloop
+				break
+			}
+			if profit < stockPricesByDay[innerloop]-stockPricesByDay[outerloop] {
+				profit = stockPricesByDay[innerloop] - stockPricesByDay[outerloop]
+			}
+
+		}
+		if profit > grandprofit {
+			grandprofit = profit
+		}
+	}
+	return grandprofit
 }
